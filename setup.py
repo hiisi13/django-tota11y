4c07d96b6a8c2b2c4a9d72526d101dcae860371a
Dmitry Kozhedubov <dmitry-kozhedubov@yandex.ru>
2015-06-12 19:44:58 -0700

    Initial implementation

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='django-tota11y',
+    version='0.1.0',
+    author='Dmitry Kozhedubov',
+    author_email='hiisi13@gmail.com',
+    url='https://github.com/hiisi13/django-tota11y',
+    packages=[
+        'tota11y',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+    ],
+    license="MIT",
+    keywords=['accessibility', 'visualization'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+)
